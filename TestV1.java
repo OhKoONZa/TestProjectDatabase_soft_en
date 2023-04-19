b626984585d4fe4ff58db8430c8f602f7164638e
OhKoONZa <ohkoonza@gmail.com>
2023-04-19 15:42:03 +0700

    yggg
    ad methods

--- TestV1.java
+++ TestV1.java
@@ -1,6 +1,11 @@
 class TestV1{
-    public void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("wut create file2");
+        kuy();
+    }
+
+    public static void kuy() {
+        System.out.println("hello wut");
     }
 }
