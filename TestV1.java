92bb41bfd550a63bd78bf9b1f905f45ae365f1c9
sittikornnn <sittikorn720@gmail.com>
2023-04-19 15:40:25 +0700

    Update TestV1.java

--- TestV1.java
+++ TestV1.java
@@ -1,5 +1,5 @@
 class TestV1{
-    public void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("wut create file2");
     }
